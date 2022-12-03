6e571c6e434e908e6a6b602ddd924fcfd9602f2d
Prostotavov <Romasenkevich@bk.ru>
2022-12-04 02:41:16 +0300

    add Title Label

--- TestTaskNotes/ViewController.swift
+++ TestTaskNotes/ViewController.swift
@@ -10,21 +10,36 @@ import UIKit
 class ViewController: UIViewController {
     
     let backHeight: CGFloat = 170
+    let backgroundViewBack = BackgroundView()
+    let backgroundViewFront = BackgroundView()
+    let titleLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupBackgroundView()
+        setupTitleLabel()
+    }
+    
+    private func setupTitleLabel() {
+        backgroundViewFront.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: backgroundViewFront.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: backgroundViewFront.centerYAnchor),
+        ])
+        titleLabel.text = "Заметки"
+        titleLabel.font = UIFont.systemFont(ofSize: 32, weight: .regular)
+        titleLabel.transform = CGAffineTransform(scaleX: -1, y: 1)
+        titleLabel.textColor = .white
     }
     
     private func setupBackgroundView() {
-        let backgroundViewBack = BackgroundView()
         backgroundViewBack.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: backHeight)
         backgroundViewBack.fillColor = Colors.LightMode.lightGreen
         backgroundViewBack.backgroundColor = .clear
         view.addSubview(backgroundViewBack)
         
-        let backgroundViewFront = BackgroundView()
         backgroundViewFront.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: backHeight)
         backgroundViewFront.transform = CGAffineTransform(scaleX: -1, y: 1)
         backgroundViewFront.fillColor = Colors.LightMode.darkGreen
